eaeca27770670258cfcb671f0b23b499f8a88557
inbard7 <66990381+inbard7@users.noreply.github.com>
2020-08-24 16:15:11 +0300

    2

--- Shnitzel.js
+++ Shnitzel.js
@@ -3561,7 +3561,7 @@ p.nominalBounds = new cjs.Rectangle(0,0,310.8,534.6);
 	// timeline functions:
 	this.frame_0 = function() {
 		this.PlayButton = this.Button.PlayButton;
-		createjs.Sound.registerSound("Shnitzel-Animation/sounds/inbar.mp3","bgmusic");
+		createjs.Sound.registerSound("/sounds/inbar.mp3","bgmusic");
 		playSound("inbar");
 		var self=this;
 		self.stop();
